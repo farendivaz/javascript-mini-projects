a74e22ac9cc4a8b8768738983a2213253838ba57
farendivaz <dandyfariz9@gmail.com>
2022-02-26 21:10:52 +0700

    add event listener

--- todos-app/app.js
+++ todos-app/app.js
@@ -4,12 +4,22 @@ const todos = document.getElementById("todos");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const todoText = input.value;
+  addTodo();
+});
 
+function addTodo() {
+  const todoText = input.value;
   if (todoText) {
     const todoEl = document.createElement("li");
     todoEl.textContent = todoText;
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+    });
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+    });
     todos.appendChild(todoEl);
     input.value = "";
   }
-});
+}
