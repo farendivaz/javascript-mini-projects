ffdb4e644bc86fda5db307aa6fc2ceb9b665f524
farendivaz <dandyfariz9@gmail.com>
2022-02-28 19:48:36 +0700

    add remove icon

--- todos-app/app.js
+++ todos-app/app.js
@@ -11,15 +11,19 @@ function addTodo() {
   const todoText = input.value;
   if (todoText) {
     const todoEl = document.createElement("li");
+    const remove = document.createElement("image");
     todoEl.textContent = todoText;
+    remove.innerHTML = `      <img src="https://img.icons8.com/ios-glyphs/25/000000/delete-forever.png" />
+    `;
     todoEl.addEventListener("click", () => {
       todoEl.classList.toggle("completed");
     });
-    todoEl.addEventListener("contextmenu", (e) => {
+    remove.addEventListener("click", (e) => {
       e.preventDefault();
       todoEl.remove();
     });
     todos.appendChild(todoEl);
+    todoEl.appendChild(remove);
     input.value = "";
   }
 }
