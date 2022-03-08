7db873d41ed47a6d735fb9abf1d17c7daf5a72d1
farendivaz <dandyfariz9@gmail.com>
2022-03-09 06:17:28 +0700

    add form

--- form-validation/script.js
+++ form-validation/script.js
@@ -1,3 +1,21 @@
 const input = document.getElementById("input");
 const password = document.getElementById("password");
 const form = document.getElementById("form");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (input.value === "" || input.value == null) {
+    messages.push("Name is required");
+  }
+  if (password.value.length <= 6) {
+    messages.push("Password must be longer than 6 characters");
+  }
+  if (password.value.length >= 6) {
+    messages.push("Password must be less than 20 characters");
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    error.textContent = messages.join(",");
+  }
+});
